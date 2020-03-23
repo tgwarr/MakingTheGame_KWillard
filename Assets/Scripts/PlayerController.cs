5d2dfd7e78e3eb5e9cf2144a2d147027c4214bdf
Kellen <k.willard@hotmail.com>
2020-03-23 11:38:35 -0700

    3A

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Rigidbody playerRigidBody;
+	public float forceMultiplier;
+	public float gravityMultiplier;
+	public bool onGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+		playerRigidBody = GetComponent<Rigidbody>();
+		Physics.gravity *= gravityMultiplier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Space) && onGround)
+		{
+			playerRigidBody.AddForce(Vector3.up * forceMultiplier, ForceMode.Impulse);
+			onGround = false;
+		}
+    }
+
+	private void OnCollisionEnter(Collision collision)
+	{
+		onGround = true;	
+	}
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+	public GameObject obstaclePrefab;
+	private Vector3 spawnLocation = new Vector3 (30, 0, 0);
+	private float initialWait = 2;
+	private float periodRep = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+		InvokeRepeating("SpawnObstacle", initialWait, periodRep);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void SpawnObstacle()
+	{
+		Instantiate (obstaclePrefab, spawnLocation, obstaclePrefab.transform.rotation);
+	}
+}
